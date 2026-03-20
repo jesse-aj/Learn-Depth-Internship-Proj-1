90b13212fa61b53242fca6de7c2d777fbc75a428
jesse-aj <appiahjesse123@gmail.com>
2026-03-20 19:08:31 +0000

    added entries for add task

--- GUI_Based_Task_Reminder/gui.py
+++ GUI_Based_Task_Reminder/gui.py
@@ -12,7 +12,7 @@ root.resizable(False, False)
 
 #Tabs
 
-notebook = ttk.Notebook(root) # linking noteebook to make everything coherant
+notebook = ttk.Notebook(root) 
 notebook.pack(fill="both", expand=True, padx=10, pady=10)
 
 #This creates all the tabs 
@@ -25,6 +25,23 @@ notebook.add(tab_add,       text="  Add Task  ")
 notebook.add(tab_view,      text="  View Tasks  ")
 notebook.add(tab_deadlines, text="  Deadlines  ")
 
+#Tab 1 - Add Task
+
+tk.Label(tab_add, text="Task Name:").grid(row=0, column=0, padx=10, pady=8, sticky="w")
+entry_name = tk.Entry(tab_add, width=40)
+entry_name.grid(row=0, column=1, padx=10, pady=8)
+
+tk.Label(tab_add, text="Description:").grid(row=1, column=0, padx=10, pady=8, sticky="w")
+entry_desc = tk.Entry(tab_add, width=40)
+entry_desc.grid(row=1, column=1, padx=10, pady=8)
+
+tk.Label(tab_add, text="Deadline (YYYY-MM-DD):").grid(row=2, column=0, padx=10, pady=8, sticky="w")
+entry_deadline = tk.Entry(tab_add, width=40)
+entry_deadline.grid(row=2, column=1, padx=10, pady=8)
+
+tk.Label(tab_add, text="Email:").grid(row=3, column=0, padx=10, pady=8, sticky="w")
+entry_email = tk.Entry(tab_add, width=40)
+entry_email.grid(row=3, column=1, padx=10, pady=8)
 
 
 
